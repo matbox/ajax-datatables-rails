5ba0437bc3ad388a50457e861efe0b5b8826c957
Matthias <matthias@matbox.net>
2015-04-25 18:14:57 -0700

    Missed loading Mongoid Extension

--- lib/ajax-datatables-rails.rb
+++ lib/ajax-datatables-rails.rb
@@ -5,6 +5,7 @@ require 'ajax-datatables-rails/base'
 require 'ajax-datatables-rails/extensions/simple_paginator'
 require 'ajax-datatables-rails/extensions/kaminari'
 require 'ajax-datatables-rails/extensions/will_paginate'
+require 'ajax-datatables-rails/extensions/mongoid'
 
 module AjaxDatatablesRails
 end
